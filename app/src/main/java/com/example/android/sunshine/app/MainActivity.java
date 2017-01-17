a1b06cc5212fa88495663899705c21dfb2e635c6
Serhii Synohub <serhii.synohub@gmail.com>
2017-01-17 21:37:54 +0200

    add array adapter

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +25,6 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-
     }
 
     @Override
@@ -54,6 +54,8 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        private ArrayAdapter<String> arrayAdapter;
+
         public PlaceholderFragment() {
         }
 
@@ -61,14 +63,21 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            ArrayList<String> fakeData = new ArrayList<>();
-            fakeData.add("Today - Sunny - 88/63");
-            fakeData.add("Tomorrow - Foggy - 70/46");
-            fakeData.add("Weds - Cloudy - 72/63");
-            fakeData.add("Thurs - Rainy - 64/51");
-            fakeData.add("Fri - Foggy - 70/46");
-            fakeData.add("Sat - Sunny - 76/68");
 
+            ArrayList<String> weekForecast = new ArrayList<>();
+            weekForecast.add("Today - Sunny - 88/63");
+            weekForecast.add("Tomorrow - Foggy - 70/46");
+            weekForecast.add("Weds - Cloudy - 72/63");
+            weekForecast.add("Thurs - Rainy - 64/51");
+            weekForecast.add("Fri - Foggy - 70/46");
+            weekForecast.add("Sat - Sunny - 76/68");
+
+            arrayAdapter = new ArrayAdapter<String>(getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weekForecast);
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(arrayAdapter);
 
             return rootView;
         }
